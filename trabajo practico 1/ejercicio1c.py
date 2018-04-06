5ece979dfbe202338396ecf29c7a79f95be658e1
mauro37ar <mauro31ar@gmail.com>
2018-04-06 14:48:14 -0300

    subiendo ejercicios hechos en clase mios y mis compañeros

---
+++ trabajo practico 1/ejercicio1c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+list = []
+camp=input("ingrese puerto: ")
+while camp!=0000:
+	if camp!=0000:
+		list.append(camp)
+	camp=input("ingrese puerto: ")
+
+print(list)
+list.sort()
+print(list)
+
+print(list.index(80))
+list.remove(443)
+print (list )
+print(len(list))
